9bdd239458163b310c0d6d337264beb00f9c1700
ffeghali <fayfeghali@protonmail.com>
2023-04-09 13:05:55 -0400

    react app frontend created with vite, react-router-dom implemented, no styling

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import {
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Route, 
+  RouterProvider
+} from 'react-router-dom'
+
+// Pages
+import Home from './pages/Home'
+import About from './pages/About'
+
+// Layouts
+import RootLayout from './layouts/RootLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App
